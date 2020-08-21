55b3e8022a151d6f88c990cbae02e66a1b8f9147
idiom-bytes <cryptoteller@gmail.com>
2020-08-21 16:26:43 -0700

    Fixing header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <p className='quote'>
             <h2>Tokens of Babel</h2>
-            <h5>A <a href='https://twitter.com/burn_the_state' target='_blank' rel='noopener noreferrer'>Bill Drummond</a> Project</h5>
+            <h5>A <a href='https://twitter.com/burn_the_state' target='_blank' rel='noopener noreferrer'>Bill Drummond</a> Experience</h5>
 
             TOB is a trading game on Ethereum that burns tokens whenever a new all time high is made.<br/>
             Inspired by the Bible story, where humanity builds a tower so high that they challenged God's divinity.
